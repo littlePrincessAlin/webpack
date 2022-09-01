f6003f33b227082e467224f897302da8145a0ca2
lilin <lilin@bailongma-inc.com>
2022-09-01 21:27:07 +0800

    feat: hmr

--- webpack-hmr/webpack.config.js
+++ webpack-hmr/webpack.config.js
@@ -1,11 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = (env) => {
   return {
-    entry: "./src/index.js",
+    mode: "development",
+    entry: {
+      app: "./src/index.js"
+    },
+    devtool: "inline-source-map",
+    // output: {
+    //   path: path.resolve(__dirname, "dist"),
+    //   filename: "[name].bundle.[contenthash].js",
+    // },
+    devServer: {
+      static: "./dist",
+      hot: true,
+      // client: false,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Hot Module Replacement",
+      }),
+      // Plugin for hot module replacement
+      // new webpack.HotModuleReplacementPlugin(),
+    ],
     output: {
+      filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
-      filename: '[name].bundle.[contenthash].js',
+      clean: true,
     },
   };
 };
